d261f2c20a6204a2ac200df49a904989179958a1
ToniPetrov03 <toniivanov03@gmail.com>
2020-01-29 14:50:43 +0200

    refactoring

--- ObjectExercise/08-unique-sequences.js
+++ ObjectExercise/08-unique-sequences.js
@@ -1,14 +1,15 @@
 function solve(arrays) {
-    return arrays.reduce((output, arrJson) => {
-        const arr = JSON.parse(arrJson);
-
-        output.push(JSON.stringify(arr.sort((a, b) => b - a)));
-
-        return output;
-    }, [])
-        .filter((v, i, a) => a.indexOf(v) === i)
-        .map(JSON.parse)
+    return arrays
+        .map(arrJson => JSON
+            .stringify(JSON
+                .parse(arrJson)
+                    .sort((a, b) => b - a)))
+        .filter((v, i, a) => a
+            .indexOf(v) === i)
+        .map(JSON
+            .parse)
         .sort((a, b) => a.length - b.length)
-        .map(arr => `[${arr.join(', ')}]`)
+        .map(arr => `[${arr
+            .join(', ')}]`)
         .join('\n');
 }
