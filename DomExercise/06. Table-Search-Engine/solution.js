d611b7ebfa34c922300ac806e9c40abafb73c226
ToniPetrov03 <toniivanov03@gmail.com>
2020-02-03 16:05:26 +0200

    06.Table Search Engine

--- DomExercise/06. Table-Search-Engine/solution.js
+++ DomExercise/06. Table-Search-Engine/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-   //TODO...
+    document.getElementById('searchBtn').addEventListener('click', onButtonClick);
+
+    const input = document.getElementById('searchField');
+    const trs = document.querySelectorAll('tbody tr');
+
+    function onButtonClick() {
+        trs.forEach(tr => {
+            tr.classList.remove('select');
+
+            if (tr.textContent.match(input.value)) {
+                tr.className = 'select';
+            }
+        });
+
+        input.value = '';
+    }
 }
