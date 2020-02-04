f2bea9c586239dca3589f2424f64d719525e6625
ToniPetrov03 <toniivanov03@gmail.com>
2020-02-04 21:35:49 +0200

    07.Shoping Cart

--- DomExercise/07. Shopping-Cart/solution.js
+++ DomExercise/07. Shopping-Cart/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-   //TODO...
+    const buttons = document.querySelectorAll('button');
+    [...buttons].forEach(button => button.addEventListener('click', onButtonClick));
+
+    const textArea = document.querySelector('textarea');
+
+    let totalPrice = 0;
+    const list = new Set();
+
+    function onButtonClick(e) {
+        if (e.target.className === 'add-product') {
+            const money = e.target.parentNode.nextElementSibling.textContent;
+            const name = e.target.parentNode.previousElementSibling.querySelector('div').textContent;
+
+            textArea.textContent += `Added ${name} for ${money} to the cart.\n`;
+
+            list.add(name);
+            totalPrice += parseFloat(money);
+        } else {
+            textArea.textContent += `You bought ${[...list].join(', ')} for ${totalPrice.toFixed(2)}.`;
+
+            [...buttons].forEach(button => button.disabled = true);
+        }
+    }
 }
