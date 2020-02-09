63f76131c660fc3e4d2528af401f464c3bc2fbc9
ToniPetrov03 <35171489+ToniPetrov03@users.noreply.github.com>
2020-02-09 16:27:17 +0200

    Exercise: Arrays (#2)

---
+++ ArraysExercise/09-diagonal-attack.js
@@ -0,0 +1,29 @@
+function solve(arrOfStrings) {
+    const matrix = arrOfStrings.map(str => str.split(' ').map(Number));
+    const [areDiagonalsEqual, sumOfDiagonal] = checkDiagonals(matrix);
+
+    return areDiagonalsEqual ? replaceMatrix(matrix, sumOfDiagonal).map(arr => arr.join(' ')).join('\n') : arrOfStrings.join('\n');
+
+    function checkDiagonals(matrix) {
+        const sumDiagonals = matrix.reduce((sumDiagonals, nums, index) => {
+            sumDiagonals.main += nums[index];
+            sumDiagonals.second += nums[matrix.length - index - 1];
+
+            return sumDiagonals;
+        }, {main: 0, second: 0});
+
+        return [sumDiagonals.main === sumDiagonals.second, sumDiagonals.main];
+    }
+
+    function replaceMatrix(matrix, replacement) {
+        return matrix.map((nums, i) => {
+            return nums.map((num, index) => {
+                if (index !== i && index !== nums.length - i - 1) {
+                    num = replacement;
+                }
+
+                return num;
+            });
+        });
+    }
+}
