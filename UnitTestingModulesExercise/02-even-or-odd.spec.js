959ded553570bbc87510a18bec6e0095c236b2cb
ToniPetrov03 <toniivanov03@gmail.com>
2020-02-12 19:55:31 +0200

    01.Request Validator and 02.Even or Odd

---
+++ UnitTestingModulesExercise/02-even-or-odd.spec.js
@@ -0,0 +1,8 @@
+const isOddOrEven = require('./02-even-or-odd');
+const expect = require('chai').expect;
+
+describe('isOddOrEven', function () {
+    it('should ', function () {
+        expect(isOddOrEven(13)).to.equal(undefined)
+    });
+});
